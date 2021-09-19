2f31eb3f45b3ac1412c4348d6df8b3bbfd8de2a1
Katherine Dubovik <dubovikES@mail.ru>
2021-09-19 20:48:41 +0300

    init

---
+++ courses/lesson3_task4.py
@@ -0,0 +1,15 @@
+# 4)Подсчет букв
+# Дано строка и буква => "fizbbbuz","b", нужно подсчитать сколько раз буква b встречается в заданной строке                                                     "Hello there", "e" == 3
+# "Hello there", "t" == 1
+# "Hello there", "h" == 2
+# "Hello there", "L" == 2
+# "Hello there", " " == 1
+
+string1 = 'fizbbbuz'
+char = 'b'
+num1 = string1.count(char)
+print(f'{string1}, {char} == {num1}')
+
+
+
+
