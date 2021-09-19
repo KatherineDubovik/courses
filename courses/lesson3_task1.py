26c207ad3e9864d16d815a774aadac376d422880
Katherine Dubovik <dubovikES@mail.ru>
2021-09-19 16:17:08 +0300

    init

---
+++ courses/lesson3_task1.py
@@ -0,0 +1,13 @@
+# 1)Из заданной строки получить нужную строку:
+
+# "String" => "SSttrriinngg"
+for i in 'String ':
+    print(i * 2, end='')
+
+# "Hello World" => "HHeelllloo  WWoorrlldd"
+for i in 'Hello World ':
+    print(i * 2, end='')
+
+# "1234!_ " => "11223344!!__  "
+for i in '1234!_':
+    print(i * 2, end='')
